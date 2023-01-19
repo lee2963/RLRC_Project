b17485310b05c5483abba36965340f559e6599ed
Narcoker <jykim9868@gmail.com>
2023-01-19 17:24:08 +0900

    관리자 로그인 페이지 반응형 작업 완료 로그인 컴포넌트 사이즈 고정, 로그인 컴포넌트가 뷰포트 중앙에 배치되도록 수정

--- frontend/rlrc/src/pages/UserLogin.js
+++ frontend/rlrc/src/pages/UserLogin.js
@@ -48,6 +48,7 @@ function UserLogin() {
       // console.log(response.data);
       setCheckLogIn(true);
     } catch (error) {
+      // setCheckLogIn(true); 로그인 스킵 코드
       setCheckLogIn(false);
     }
   };
@@ -65,68 +66,94 @@ function UserLogin() {
       {checkLogin ? (
         <PathSelection />
       ) : (
-        <Container>
-          <Image src={loginImage}></Image>
-          <LoginForm onSubmit={handleSubmit}>
-            <IdLabel htmlFor="userId">ID</IdLabel>
-            <IdInput
-              type="text"
-              id="userId"
-              onChange={handleId}
-              value={userId}
-            />
-            <LineId />
-            <br />
-            <PasswordLabel htmlFor="password" value={password}>
-              Password
-            </PasswordLabel>
-            <PasswordInput
-              id="password"
-              type="password"
-              onChange={handlePassword}
-            />
-            <LinePassword />
-            <Button type="submit">Login</Button>
-          </LoginForm>
-        </Container>
+        <MainContaniner>
+          <Container>
+            <Image src={loginImage}></Image>
+            <LoginForm onSubmit={handleSubmit}>
+              <InputWrapper>
+                <IdLabel htmlFor="userId">ID</IdLabel>
+                <IdInput
+                  type="text"
+                  id="userId"
+                  onChange={handleId}
+                  value={userId}
+                />
+              </InputWrapper>
+
+              <InputWrapper>
+                <PasswordLabel htmlFor="password" > Password </PasswordLabel>
+                <PasswordInput
+                  id="password"
+                  type="password"
+                  onChange={handlePassword}
+                  value={password}
+                />
+              </InputWrapper>
+              <Button type="submit">Login</Button>
+            </LoginForm>
+          </Container>
+        </MainContaniner>
       )}
     </>
   );
 }
 
+const MainContaniner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
 const Container = styled.main`
-  position: absolute;
-  top: 90px;
-  left: 18%;
   width: 1190px;
   height: 738px;
   background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
   background: #ffffff 0% 0% no-repeat padding-box;
   box-shadow: 3px 7px 33px #00000029;
   opacity: 1;
+
+  display: flex;
+  flex-direction: row;
 `;
 const Image = styled.img`
-  position: relative;
+  // position: relative;
   width: (1190 / 2) px;
   height: 738px;
 `;
 
 const LoginForm = styled.form`
-  position: absolute;
-  top: 200px;
-  left: 580px;
-  height: 200px;
-  width: 50%;
+  // position: absolute;
+  // top: 200px;
+  // left: 580px;
+  // display: flexbox;
+  // align-items: center;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  display: flexbox;
+  height: 100%;
+  width: 100%;
+  
 `;
 
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  border-bottom: 2px solid #2680eb;
+  margin-bottom: 30px;
+  width: 391px;
+`;
+
+
 const IdInput = styled.input`
-  position: relative;
-  top: -3px;
-  width: 200px;
+  // position: relative;
+  // top: -3px;
+  // left: 140px;
+  width: 100%;
   height: 35px;
-  left: 140px;
   text-align: left;
   font: normal normal 25px sans-serif;
   letter-spacing: 0px;
@@ -139,21 +166,25 @@ const IdInput = styled.input`
   border: none;
 `;
 const IdLabel = styled.label`
-  position: relative;
-  left: 118px;
-  width: 17px;
+  // position: relative;
+  // left: 118px;
+  // width: 17px;
+
+
   letter-spacing: var(--unnamed-character-spacing-0);
   text-align: left;
   font: normal normal 25px sans-serif;
   letter-spacing: 0px;
   color: #2680eb;
   opacity: 1;
+  margin-right: 36px;
 `;
 const PasswordInput = styled.input`
-  position: relative;
-  top: 73px;
-  left: 140px;
-  width: 200px;
+  // position: relative;
+  // top: 73px;
+  // left: 140px;
+
+  width: 100%;
   height: 35px;
   text-align: left;
   font: normal normal 25px sans-serif;
@@ -167,41 +198,22 @@ const PasswordInput = styled.input`
   border: none;
 `;
 const PasswordLabel = styled.label`
-  position: relative;
-  left: 118px;
-  top: 75px;
-  width: 80px;
-  height: 21px;
   font: normal normal 25px sans-serif;
   letter-spacing: 0px;
   color: #2680eb;
   opacity: 1;
+  margin-right: 36px;
 `;
-const LineId = styled.span`
-  position: absolute;
-  top: 110px;
-  left: 120px;
-  background-color: #2680eb;
-  width: 350px;
-  height: 2px;
-`;
-const LinePassword = styled.span`
-  position: absolute;
-  top: 220px;
-  left: 120px;
-  background-color: #2680eb;
-  width: 350px;
-  height: 2px;
-`;
+
 const Button = styled.button`
-  position: relative;
-  top: 210px;
-  right: 110px;
+  // position: relative;
+  // top: 210px;
+  // right: 110px;
+  margin-top: 60px;
   width: 176px;
   height: 43px;
   background: #2576f8 0% 0% no-repeat padding-box;
   opacity: 1;
-
   font: var(--unnamed-font-style-normal) normal medium 18px/21px
     var(--unnamed-font-family-sans-serif);
   letter-spacing: var(--unnamed-character-spacing-0);
