5c68c707fb5861e8d7eb267629ae8c1b96c45f98
lee2963 <sodel2963@naver.com>
2023-01-24 14:40:29 +0900

    admin 초기 데이터 수정

--- src/main/java/RLRC/rlrc/RlrcApplication.java
+++ src/main/java/RLRC/rlrc/RlrcApplication.java
@@ -1,13 +1,29 @@
 package RLRC.rlrc;
 
+import RLRC.rlrc.admin.Admin;
+import RLRC.rlrc.admin.AdminRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RlrcApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(RlrcApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(AdminRepository adminRepository) {
+		return args -> {
+			Admin admin = new Admin();
+			admin.setLoginId("admin");
+			admin.setPassword("1q2w3e4r!");
+
+			adminRepository.save(admin);
+		};
+	}
 }
